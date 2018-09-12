67f26b7ae1391c700778f4d345b66af94608060b
JAZI <2322492240@qq.com>
2018-09-12 10:23:34 +0800

    'resumed-demo'

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <layOut></layOut>
+  </div>
+</template>
+
+<script>
+import LayOut from './components/LayOut'
+
+export default {
+  components: {
+    'layOut': LayOut
+  }
+}
+</script>
+
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+ul,ol,li{
+  list-style: none;
+}
+a{
+  text-decoration: none;
+  color: #333;
+}
+</style>
+
---
+++ src/components/LayOut.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="layout">
+    <left></left>
+    <right></right>
+  </div>
+</template>
+
+<script>
+import Left from './Left'
+import Right from './Right'
+
+export default {
+  components: {
+    'left': Left,
+    'right': Right
+  }
+}
+</script>
+
+<style lang="scss">
+.layout{
+  font-family: "Avenir","Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
+  color: #333;
+  font-size: 16px;
+  width: 1100px;
+  margin: 30px auto;
+  padding: 0 30px;
+  padding-bottom: 30px;
+  background-color: #dfdfdf;
+  border-radius: 20px;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
+
